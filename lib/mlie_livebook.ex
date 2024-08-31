937abc04040c96a550be28ec438b74ec9a57d2f1
Justin Giancola <justin.giancola@gmail.com>
2024-08-31 12:09:07 +0000

    mix new

---
+++ lib/mlie_livebook.ex
@@ -0,0 +1,18 @@
+defmodule MlieLivebook do
+  @moduledoc """
+  Documentation for `MlieLivebook`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MlieLivebook.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mlie_livebook_test.exs
@@ -0,0 +1,8 @@
+defmodule MlieLivebookTest do
+  use ExUnit.Case
+  doctest MlieLivebook
+
+  test "greets the world" do
+    assert MlieLivebook.hello() == :world
+  end
+end
